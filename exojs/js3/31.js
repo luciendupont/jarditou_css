1167a93e37359a9b2a4c3013cfac70ca87169cf6
luciendupont <115162613+luciendupont@users.noreply.github.com>
2022-10-20 13:47:38 +0200

    Delete exojs directory

--- exojs/js3/31.js
+++
@@ -1,9 +0,0 @@
-var pair =window.prompt("verifier le nombre pair ");
-if (pair%2==0){
-
-    alert("nombre pair ");
-    
-}
-else(
-    alert("nombre impair")
-);
--- exojs/js4/4.js
+++
@@ -1,7 +0,0 @@
-window.prompt("prenom");
-while (i=prompt);
-{
-
-    i+1(true) 
-    i+0(false)
-};
