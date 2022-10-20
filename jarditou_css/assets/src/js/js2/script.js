fca91f2318bca3d7d947c7d2a40b8e364a8f4492
luciendupont <115162613+luciendupont@users.noreply.github.com>
2022-10-20 11:11:09 +0200

    jardito-csss

---
+++ jarditou_css/assets/src/js/js2/script.js
@@ -0,0 +1,8 @@
+var a="100"
+alert("ceci est une chaine de caracter"+a);
+var b=100 
+alert (b--);
+var c=1.00
+alert(parseInt(a)+c);
+var d=true
+alert(d=false);
---
+++ jarditou_css/assets/src/js/js3/31.js
@@ -0,0 +1,9 @@
+var pair =window.prompt("verifier le nombre pair ");
+if (pair%2==0){
+
+    alert("nombre pair ");
+    
+}
+else(
+    alert("nombre impair")
+);
