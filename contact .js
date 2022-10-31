0e69c89dd863706cf31a437dcabc1818df3b9538
luciendupont <lucienluludupont@gmail.com>
2022-10-31 13:48:56 +0100

    eval

--- contact .js
+++
@@ -1,4 +0,0 @@
-let champ_nom = form_inscription.elements["nom"];
-let champ_prenom = form_inscription.elements["prenom"];
-let champ_telephone = form_inscription.elements["telephone"];
-let champ_email = form_inscription.elements["mail"];
